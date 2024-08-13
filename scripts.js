314391f58c6d1eb33ea3f14fa07223a251610c39
F3lipeSouza <felipevieira67.fs@gmail.com>
2024-08-12 22:00:04 -0300

    começando os scripts

--- scripts.js
+++ scripts.js
@@ -1,30 +1,50 @@
 
 const formulario = document.querySelector('form');
+const produto = formulario.querySelector('#inputproduto');
+const quantidade = formulario.querySelector('#inputquantidade');
+const valor = formulario.querySelector('#inputvalor');
 let consumidos = [];
 
-formulario.addEventListener('submit', (event) =>{
-
-    event.preventDefault();
-
-    //capturar elementos dos inputs do formulario;
-    let produto = formulario.querySelector('#inputproduto');
-    let quantidade = formulario.querySelector('#inputquantidade');
-    let valor = formulario.querySelector('#inputvalor');
-
-    //validar informações do formulario;
-
-    if(produto.value == ''){
-        alert('Informe o produto!');
+function entradaVazia (entrada, mensagem){
+    if(entrada.value === ''){
+        alert(mensagem);
+        return true;
     }
+    return false;
+}
 
-    if(quantidade.value == ''){
-        alert('Informe a quantidade!');
+function validaDados (){
+
+    if(
+        entradaVazia (produto, 'Informe um produto!') ||
+        entradaVazia (quantidade, 'Informe a quantidade!') ||
+        entradaVazia (valor, 'Informe um valor')    
+    ){
+        return true;
     }
+    return false;
     
-    if(valor.value == ''){
-    alert('Informe o valor!');
-    }
+}
 
+
+const creatTable = () =>{
+    let tdQuant = document.createElement('td');
+    tdQuant.innerHTML = consumidos.quantidade;
     
 
-} );
+
+    console.log(tdQuant);
+}
+
+ 
+
+formulario.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    if(validaDados() === false ){
+
+    consumidos.push({produto:produto.value, quantidade:quantidade.value, valor:valor.value});
+    console.log(consumidos);
+    }
+    creatTable();
+});
