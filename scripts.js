41c06d4f163340e92be017386d2d72a9b6e8347b
Felipe Souza <felipevieira67.fs@gmail.com>
2024-09-19 20:09:50 -0300

    resolve bug da atualização dos ids dos botões e consumidos.

--- scripts.js
+++ scripts.js
@@ -4,13 +4,12 @@ const formulario = document.querySelector('form');
 const produto = formulario.querySelector('#inputproduto');
 const quantidade = formulario.querySelector('#inputquantidade');
 const valor = formulario.querySelector('#inputvalor');
-const tabela = document.querySelector('table');
+const tabela = document.querySelector('.tabela');
 let total = 0;
 const valorTotal = document.querySelector('#total'); 
 
 //cria lista dos produtos que foram consumidos;
 let consumidos = JSON.parse(localStorage.getItem('consumidos')) || Array();
-
 
 //verifica se entradas foram preenchidas;
 function entradaVazia (entrada, mensagem){
@@ -38,7 +37,7 @@ function validaDados (){
 const tagTd = (inf)=>{
     document.createElement('td');
     innerHTML = consumidos[consumidos.length -1].inf;
-    return inf.value
+    return inf
 }
 
 //cria tabela dinamica de produtos consumidos;
@@ -51,7 +50,6 @@ const creatTable = (quant, prod, val) =>{
 
     let btnApaga = document.createElement('button');
     btnApaga.innerHTML = 'X';
-    btnApaga.id = consumidos.length;
     btnApaga.addEventListener('click', apagaConsumo);
 
     //lança na tela dados de consumidos;
@@ -59,7 +57,6 @@ const creatTable = (quant, prod, val) =>{
     tdApaga.append(btnApaga);
 
     let postTable = document.createElement('tr');
-    postTable.id = 'tr' + consumidos.length;
     postTable.append(tdQuant, tdProduto, tdValor, tdApaga);
 
     let table = document.querySelector('.tabela');
@@ -77,8 +74,9 @@ formulario.addEventListener('submit', (evento) => {
 
     consumidos.push({id:consumidos.length +1, produto:produto.value, quantidade:quantidade.value, valor:valor.value});
     localStorage.setItem("consumidos", JSON.stringify(consumidos));
-    creatTable(quantidade, produto, valor);
+    creatTable(quantidade.value, produto.value, valor.value);
     somaTotal();
+    atualizaId();
     formulario.reset();
     return consumidos
     }
@@ -100,29 +98,59 @@ const somaTotal = ()=>{
 
 
 
+//apaga da memoria dados salvos de consumidos;
 const apagaConsumo = (b) => {
-    //apaga da memoria dados salvos de consumidos;
     let bId = b.target.id;
-    console.log(consumidos);
 
-    for(let i = 0; i < consumidos.length; i++)
+    for(let i = 0; i < consumidos.length; i++){
         if(consumidos[i].id == bId){
             //subtrai valor na soma total;
             let converteVirgula = consumidos[i].valor.replace(',', '.');
-            valorTotal.innerHTML = '';
             total -= consumidos[i].quantidade * converteVirgula;
-            console.log(total);
             valorTotal.innerHTML = total;
             
             consumidos.splice(i, 1);
-            
-            //apaga da tela dados slavos de consumidos;
-            let apagaTr = document.querySelector('#tr'+ bId)
-            
-            console.log(consumidos);
-            tabela.removeChild(apagaTr);
-            return total
+            localStorage.setItem('consumidos', JSON.stringify(consumidos));
+          
+            console.log(localStorage.getItem('consumidos'));
 
+            //apaga da tela dados slavos de consumidos;
+            let tr = document.getElementById(bId);
+            
+            tabela.removeChild(tr);
+            
+            atualizaId();
+             
+            return total
+            
         }
-        
+    }    
 }
+
+//atualiza o id dos botões e tr's;
+const atualizaId = () =>{
+    let botões = document.querySelectorAll('button');
+    let tr = document.querySelectorAll('tr');
+
+    for(let i = 0; i < botões.length; i++){
+        botões[i].id =  i + 1;
+        tr[i].id = i + 1;
+    }
+    for(let i = 0; i < consumidos.length; i++){
+        consumidos[i].id = i +1;
+    }
+}
+
+//inicia aplicaçao com valores salvos do navegador;
+const iniciaValores = () =>{
+    if(consumidos){
+        for(let i = 0; i < consumidos.length; i++){
+            creatTable(consumidos[i].quantidade, consumidos[i].produto, consumidos[i].valor);
+            somaTotal();
+            atualizaId();
+        }
+    }
+}
+iniciaValores()
+
+//localStorage.clear();
