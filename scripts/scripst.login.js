58060ac1140305929d0a1042b8a781c09ccec75a
Felipe_Souza <felipevieira67.fs@gmail.com>
2025-01-10 22:31:02 -0300

    adiciona no back end a tarefa de conferir dados para login de usuario

--- scripts/scripst.login.js
+++ scripts/scripst.login.js
@@ -15,15 +15,26 @@ const button = form.querySelector('#cadastreSe');
 
 //certifia que email e senha estão corretos;
 let logado = async () => {
-    const inputEmail = document.querySelector('#loginEmail');
-    const inputSenha = document.querySelector('#loginSenha');
+    const inputEmail = document.querySelector('#loginEmail').value;
+    const inputSenha = document.querySelector('#loginSenha').value;
 
-    if(email === inputEmail.value && senha === inputSenha.value){
-        sessionStorage.setItem('logado', true);
-        window.location.href = 'index.html';
-    }else{
-        alert('usuario ou senha incorretos!');
-    };
+
+    try{
+        const response = await fetch('http://localhost:3000/login', {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({inputEmail, inputSenha})
+        })    
+        const data = await response.json();
+        if(data.success){
+            sessionStorage.setItem('logado', true);
+            window.location.href = 'index.html';
+        }else{
+            alert('usuario ou senha incorretos!')
+        }
+    }catch{}        
 }
 
 
